3c9b9400f65e79bc83c99b78e651de4a817f2c7a
Vlad <vampir2204@gmail.com>
2024-01-18 22:02:52 +0300

    feat: done second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+rl.write('write your text\n');
+rl.on('line', (text) => {
+  if (text === 'exit') return rl.close();
+  writeStream.write(text + '\n');
+});
+rl.on('close', () => {
+  input.write('\nprogram is ended');
+});
